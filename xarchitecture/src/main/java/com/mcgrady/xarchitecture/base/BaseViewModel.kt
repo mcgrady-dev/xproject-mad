1c4ada74b98b1424bd843bc965fb8b0e86908e41
mcgrady <mcgrady911@foxmail.com>
2022-04-13 15:44:45 +0800

    feat(xarchitecture): add databinding viewholder

--- xarchitecture/src/main/java/com/mcgrady/xarchitecture/base/BaseViewModel.kt
+++ xarchitecture/src/main/java/com/mcgrady/xarchitecture/base/BaseViewModel.kt
@@ -1,6 +1,18 @@
 package com.mcgrady.xarchitecture.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+
 /**
  * Created by mcgrady on 2022/1/7.
- */class BaseViewModel {
+ */
+abstract class BaseViewModel : ViewModel() {
+
+    fun <T> Flow<T>.asLiveDataOnViewModelScope(): LiveData<T> {
+        return asLiveData(viewModelScope.coroutineContext + Dispatchers.IO)
+    }
 }
