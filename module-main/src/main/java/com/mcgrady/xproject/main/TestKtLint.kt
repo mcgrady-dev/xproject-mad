3995b8bc15478f7c2e02e5eaaf0784c85c7ea093
mcgrady <mcgrady911@foxmail.com>
2022-06-01 16:33:01 +0800

    style: code formatter

--- module-main/src/main/java/com/mcgrady/xproject/main/TestKtLint.kt
+++ module-main/src/main/java/com/mcgrady/xproject/main/TestKtLint.kt
@@ -1,28 +1,42 @@
+/*
+ * Copyright 2022 mcgrady
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mcgrady.xproject.main
 
 /**
  * Created by mcgrady on 2022/6/1.
  */
 fun
-        f  (
-    a : Int
-    , b: Double , c:String) {           var result = 0
+f(
+    a: Int,
+    b: Double,
+    c: String
+) {
+    var result = 0
     val aVeryLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongVar = 43
-    foo.bar.zed.accept(
+    foo.bar.zed.accept()
 
-    )
-
-    foo(
-
-    )
+    foo()
 
     foo.bar.zed.accept(
         DoSomething.bar()
     )
 
     bar(
-        ImmutableList.newBuilder().add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).build())
-
+        ImmutableList.newBuilder().add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).build()
+    )
 
     ImmutableList.newBuilder().add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).build()
 }
