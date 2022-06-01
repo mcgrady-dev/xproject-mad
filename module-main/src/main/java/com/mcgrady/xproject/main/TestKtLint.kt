da155208b175c80be540642b7a62d28c06bbe61d
mcgrady <mcgrady911@foxmail.com>
2022-06-01 16:32:15 +0800

    test(main): git hooks spotless

---
+++ module-main/src/main/java/com/mcgrady/xproject/main/TestKtLint.kt
@@ -0,0 +1,28 @@
+package com.mcgrady.xproject.main
+
+/**
+ * Created by mcgrady on 2022/6/1.
+ */
+fun
+        f  (
+    a : Int
+    , b: Double , c:String) {           var result = 0
+    val aVeryLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongVar = 43
+    foo.bar.zed.accept(
+
+    )
+
+    foo(
+
+    )
+
+    foo.bar.zed.accept(
+        DoSomething.bar()
+    )
+
+    bar(
+        ImmutableList.newBuilder().add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).build())
+
+
+    ImmutableList.newBuilder().add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).add(1).build()
+}
