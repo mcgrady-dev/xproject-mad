07cd5224171c9107a9caf0b61d72b8ee5fd2527a
mcgrady <mcgrady911@foxmail.com>
2023-08-15 11:35:51 +0800

    refactor(pokemon): 重构pokemon模块

---
+++ data/pokemon/src/main/java/com/mcgrady/xproject/data/pokemon/database/util/TypeResponseConverter.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 mcgrady
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mcgrady.xproject.data.pokemon.database.util
+
+import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import com.mcgrady.xproject.data.pokemon.model.PokemonInfo
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
+import kotlin.Exception
+
+@ProvidedTypeConverter
+class TypeResponseConverter @Inject constructor(private val json: Json) {
+
+    @TypeConverter
+    fun fromString(value: String): List<PokemonInfo.TypeResponse>? {
+        return try {
+            json.decodeFromString(
+                deserializer = ListSerializer(PokemonInfo.TypeResponse.serializer()),
+                string = value,
+            )
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    @TypeConverter
+    fun fromInfoType(type: List<PokemonInfo.TypeResponse>?): String {
+        return try {
+            json.encodeToString(type)
+        } catch (e: Exception) {
+            ""
+        }
+    }
+}
---
+++ data/pokemon/src/main/java/com/mcgrady/xproject/data/pokemon/model/PokemonInfo.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 mcgrady
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mcgrady.xproject.data.pokemon.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlin.random.Random
+
+@Serializable
+data class PokemonInfo(
+    val id: Int,
+    val name: String,
+    val height: Int,
+    val weight: Int,
+    @SerialName("base_experience") val experience: Int,
+    val types: List<TypeResponse>,
+    val hp: Int = Random.nextInt(maxHp),
+    val attack: Int = Random.nextInt(maxAttack),
+    val defense: Int = Random.nextInt(maxDefense),
+    val speed: Int = Random.nextInt(maxSpeed),
+    val exp: Int = Random.nextInt(maxExp),
+) {
+
+    fun getIdString(): String = String.format("#%03d", id)
+    fun getWeightString(): String = String.format("%.1f KG", weight.toFloat() / 10)
+    fun getHeightString(): String = String.format("%.1f M", height.toFloat() / 10)
+    fun getHpString(): String = " $hp/$maxHp"
+    fun getAttackString(): String = " $attack/$maxAttack"
+    fun getDefenseString(): String = " $defense/$maxDefense"
+    fun getSpeedString(): String = " $speed/$maxSpeed"
+    fun getExpString(): String = " $exp/$maxExp"
+
+    @Serializable
+    data class TypeResponse(val slot: Int, val type: Type)
+
+    @Serializable
+    data class Type(val name: String)
+
+    companion object {
+        const val maxHp = 300
+        const val maxAttack = 300
+        const val maxDefense = 300
+        const val maxSpeed = 300
+        const val maxExp = 1000
+    }
+}
